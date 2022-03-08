4bc9a9245c019997425ebd4f90ffd683eb40040d
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-03-09 03:21:09 +0545

    Add password helper

--- src/app/components/Register/Register.module.scss.d.ts
+++ src/app/components/Register/Register.module.scss.d.ts
@@ -3,3 +3,6 @@ export const form: string;
 export const formTitle: string;
 export const formDescription: string;
 export const registerBtn: string;
+export const helperSection: string;
+export const passwordRulesList: string;
+export const passwordRule: string;
--- src/app/components/Register/Register.tsx
+++ src/app/components/Register/Register.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import InputLabelGroup from "app/components/InputLabelGroup/InputLabelGroup";
 import styles from "./Register.module.scss";
 import PrimaryButton from "../Buttons/PrimaryButton/PrimaryButton";
+import { GrStatusGood, GrFormClose } from "react-icons/gr";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
 const Register = () => {
@@ -10,6 +11,58 @@ const Register = () => {
     email: "",
     phone: "",
     password: "",
+  };
+  const [satisfyUppercase, setSatisfyUppercase] = useState(false);
+  const [satisfyLowercase, setSatisfyLowercase] = useState(false);
+  const [satisfyNumber, setSatisfyNumber] = useState(false);
+  const [satisfySpecialChar, setSatisfySpecialChar] = useState(false);
+  const [satisfy8Chars, setSatisfy8Chars] = useState(false);
+
+  const checkUppercase = (text: string) => {
+    return /[A-Z]+/.test(text);
+  };
+  const checkLowercase = (text: string) => {
+    return /[a-z]+/.test(text);
+  };
+  const checkNumber = (text: string) => {
+    return /[0-9]+/.test(text);
+  };
+  const checkSpecialChars = (text: string) => {
+    return /[@$#!%.*?&]+/.test(text);
+  };
+  const check8Chars = (text: string) => {
+    return text.length >= 8;
+  };
+
+  const checkPasswordRequirements = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    let value = e.target.value;
+    if (checkUppercase(value)) {
+      setSatisfyUppercase(true);
+    } else {
+      setSatisfyUppercase(false);
+    }
+    if (checkLowercase(value)) {
+      setSatisfyLowercase(true);
+    } else {
+      setSatisfyLowercase(false);
+    }
+    if (checkNumber(value)) {
+      setSatisfyNumber(true);
+    } else {
+      setSatisfyNumber(false);
+    }
+    if (checkSpecialChars(value)) {
+      setSatisfySpecialChar(true);
+    } else {
+      setSatisfySpecialChar(false);
+    }
+    if (check8Chars(value)) {
+      setSatisfy8Chars(true);
+    } else {
+      setSatisfy8Chars(false);
+    }
   };
 
   const validationSchema = Yup.object({
@@ -28,7 +81,7 @@ const Register = () => {
       .min(8, "Password must be at least 8 characters.")
       .max(32, "Password can't be more than 32 characters.")
       .matches(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$#!%.*?&])[A-Za-z\d@$#!%*.?&]{8,}$/,
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[ @$#!%.*?&])[A-Za-z\d @$#!%*.?&]{8,}$/,
         "Password is not valid."
       )
       .required("Password is required."),
@@ -69,9 +122,35 @@ const Register = () => {
             label="Password"
             type="password"
             required={true}
+            onKeyUp={checkPasswordRequirements}
           />
           <PrimaryButton type="submit">Register</PrimaryButton>
         </Form>
+        <section className={styles.helperSection}>
+          <h3>Password requirements</h3>
+          <ul className={styles.passwordRulesList}>
+            <li className={styles.passwordRule}>
+              {satisfyUppercase ? <GrStatusGood /> : <GrFormClose />}
+              At least 1 uppercase letter
+            </li>
+            <li className={styles.passwordRule}>
+              {satisfyLowercase ? <GrStatusGood /> : <GrFormClose />}
+              At least 1 lowercase letter
+            </li>
+            <li className={styles.passwordRule}>
+              {satisfySpecialChar ? <GrStatusGood /> : <GrFormClose />}
+              At least 1 special character
+            </li>
+            <li className={styles.passwordRule}>
+              {satisfyNumber ? <GrStatusGood /> : <GrFormClose />}
+              At least 1 number
+            </li>
+            <li className={styles.passwordRule}>
+              {satisfy8Chars ? <GrStatusGood /> : <GrFormClose />}
+              Must be at least 8 characters long
+            </li>
+          </ul>
+        </section>
       </div>
     </Formik>
   );
