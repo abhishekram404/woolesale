761d4f580d1416cb5c0fe05fdb335bb8b5cd27a2
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-04-07 18:50:17 +0545

    Add password reset feature

--- src/app/Interfaces/IPasswordReset.ts
+++ src/app/Interfaces/IPasswordReset.ts
@@ -1,3 +1,3 @@
-export interface IPasswordReset{
-
-                      }
+export interface IPasswordReset {
+  email: string;
+}
--- src/app/components/PasswordReset/PasswordReset.tsx
+++ src/app/components/PasswordReset/PasswordReset.tsx
@@ -1,26 +1,55 @@
 import React from "react";
 import styles from "app/components/Register/Register.module.scss";
-import inputStyles from "../InputLabelGroup/InputLabelGroup.module.scss";
 import PrimaryButton from "../Buttons/PrimaryButton/PrimaryButton";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { auth } from "app/firebase/firebase";
+import { Form, Formik } from "formik";
+import InputLabelGroup from "../InputLabelGroup/InputLabelGroup";
+import { toast } from "react-toastify";
+import { IPasswordReset } from "app/Interfaces/IPasswordReset";
 const PasswordReset = React.memo(() => {
+  const handleSubmit = (values: IPasswordReset) => {
+    console.log(values);
+    resetPassword(values.email);
+  };
+
+  const resetPassword = async (email: string) => {
+    try {
+      toast.promise(
+        async () => {
+          await sendPasswordResetEmail(auth, email);
+        },
+        {
+          success: "Password reset link sent to your email.",
+          pending: "Sending password reset link...",
+          error: {
+            render({ data }: { data: { message: string } }) {
+              return data.message
+                .replace("Firebase: ", "")
+                .replace("auth/", "");
+            },
+          },
+        }
+      );
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  };
   return (
-    <div className={styles.register}>
-      <div className={styles.form}>
-        <h2 className={styles.formTitle}>Reset password</h2>
-        <p className={styles.formDescription}>
-          You can reset your password in case you forgot it.
-        </p>
+    <Formik initialValues={{ email: "" }} onSubmit={handleSubmit}>
+      <div className={styles.register}>
+        <Form className={styles.form}>
+          <h2 className={styles.formTitle}>Reset password</h2>
+          <p className={styles.formDescription}>
+            You can reset your password in case you forgot it.
+          </p>
 
-        <div className={inputStyles.inputLabelGroup}>
-          <label htmlFor="email" className={inputStyles.label}>
-            Email address
-          </label>
-          <input type="email" id="email" className={inputStyles.input} />
-        </div>
+          <InputLabelGroup name="email" label="Email address" />
 
-        <PrimaryButton type="submit">Send password reset link</PrimaryButton>
+          <PrimaryButton type="submit">Send password reset link</PrimaryButton>
+        </Form>
       </div>
-    </div>
+    </Formik>
   );
 });
 export default PasswordReset;
