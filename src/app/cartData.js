37bcfbaad460435cdb4b6c5928595fbd7b8d3e53
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-12-25 11:21:04 +0545

    feat: successfully updated to sass

--- src/app/cartData.js
+++ src/app/cartData.js
@@ -1,84 +1,93 @@
-import macbook1 from "./assets/mac.jpg";
-import macbook2 from "./assets/mac2.jpg";
-import shoes from "./assets/shoe.jfif";
-import airpods from "./assets/airpods1.jpg";
-import airpods2 from "./assets/airpods2.jpg";
-import airpods3 from "./assets/airpods3.jpg";
-import airpods4 from "./assets/airpods4.jpg";
-import airpods5 from "./assets/airpods5.jpg";
-import airpods6 from "./assets/airpods6.jpg";
-import phone from "./assets/phone.jfif";
-import applePencil from "./assets/applepencil.jpg";
-import keyboard from "./assets/keyboard.jpg";
-import keyboard2 from "./assets/keyboard2.jpg";
-import keyboard3 from "./assets/keyboard3.jpg";
-import keyboard4 from "./assets/keyboard4.jpg";
-import keyboard5 from "./assets/keyboard5.jpg";
-import keyboard6 from "./assets/keyboard6.jpg";
-import keyboard7 from "./assets/keyboard7.jpg";
-import hardDrive from "./assets/harddrive.jpg";
-import hardDrive2 from "./assets/harddrive2.jpg";
-import ipad from "./assets/ipad 20inch 2021.jpg";
-import ipa2 from "./assets/ipad air 2020 10.9.jpg";
-import ipa3 from "./assets/ipad2 20inch 2021 .jpg.jpg";
-import ipa4 from "./assets/ipda air 10.9inch 2020.jpg";
+import { v4 as uuidv4 } from "uuid";
 export const cart = [
   {
+    id: uuidv4(),
     name: "Macbook pro 2021",
     price: 1499,
-    productImages: [macbook1, macbook2],
+    productImages: ["/images/mac.jpg", "/images/mac2.jpg"],
   },
   {
+    id: uuidv4(),
+
     name: "Puma Sneakers",
     price: 399,
-    productImages: [shoes],
+    productImages: ["/images/shoe.jfif"],
   },
   {
+    id: uuidv4(),
+
     name: "Apple Airpods",
     price: 699,
-    productImages: [airpods, airpods2, airpods3, airpods4, airpods5, airpods6],
-  },
-  {
-    name: "Canon Professional quality camera",
-    price: 839,
-    productImages: [],
-  },
-  {
-    name: "Oneplus Nord",
-    price: 459,
-    productImages: [phone],
-  },
-  {
-    name: "Apple Pencil",
-    price: 299,
-    productImages: [applePencil],
-  },
-  {
-    name: "Gaming Keyboard with RGB lights",
-    price: 129,
     productImages: [
-      keyboard,
-      keyboard2,
-      keyboard3,
-      keyboard4,
-      keyboard5,
-      keyboard6,
-      keyboard7,
+      "/images/airpods1.jpg",
+      "/images/airpods2.jpg",
+      "/images/airpods3.jpg",
+      "/images/airpods4.jpg",
+      "/images/airpods5.jpg",
+      "/images/airpods6.jpg",
     ],
   },
   {
+    id: uuidv4(),
+
+    name: "Canon Professional quality camera",
+    price: 839,
+    productImages: ["/images/camera.jpg"],
+  },
+  {
+    id: uuidv4(),
+
+    name: "Oneplus Nord",
+    price: 459,
+    productImages: ["/images/phone.jfif"],
+  },
+  {
+    id: uuidv4(),
+
+    name: "Apple Pencil",
+    price: 299,
+    productImages: ["/images/applepencil.jpg"],
+  },
+  {
+    id: uuidv4(),
+
+    name: "Gaming Keyboard with RGB lights",
+    price: 129,
+    productImages: [
+      "/images/keyboard.jpg",
+      "/images/keyboard2.jpg",
+      "/images/keyboard3.jpg",
+      "/images/keyboard4.jpg",
+      "/images/keyboard5.jpg",
+      "/images/keyboard6.jpg",
+      "/images/keyboard7.jpg",
+    ],
+  },
+  {
+    id: uuidv4(),
+
     name: "Hard Drive",
     price: 350,
-    productImages: [hardDrive, hardDrive2],
+    productImages: ["/images/harddrive.jpg", "/images/harddrive2.jpg"],
   },
   {
+    id: uuidv4(),
+
     name: "IPad air 2021",
     price: 799,
-    productImages: [ipad, ipa3],
+    productImages: [
+      "/images/ipad 20inch 2021.jpg",
+      "/images/ipad2 20inch 2021 .jpg.jpg",
+    ],
   },
   {
+    id: uuidv4(),
+
     name: "IPad air 2020 10.9 inch",
     price: 699,
-    productImages: [ipa2, ipa4],
+    productImages: [
+      "/images/ipad air 2020 10.9.jpg",
+      "/images/ipda air 10.9inch 2020.jpg",
+    ],
   },
 ];
